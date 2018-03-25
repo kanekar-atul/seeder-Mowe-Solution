3d2e39dd1f00c95a7d2fa38cc90c69c977f85605
Mohamed Emad <m.emad550@gmail.com>
2018-03-25 10:08:13 +0200

    it's first commit for this project and this commit is just to upload the project to GITHUB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DataTableComponent } from './data-table/data-table.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule
